4531bd58ec644c5d06bf59f66189cffc68e835fa
Liam Skop <ljs773@cs.bham.ac.uk>
2019-06-14 16:05:31 +0100

    added placeholder titlescreen

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
+var velocity : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,3 +13,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
+func _physics_process(delta):
+	if Input.is_action_pressed("left"):
+		velocity.x = -1
+	if Input.is_action_pressed("right"):
+		velocity.x = 1
+	velocity = move_and_slide(velocity*200)
+	
